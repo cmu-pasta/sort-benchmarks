4537f4643a76030e14f8d2fbc9253565fdd2dcd5
Vasu Vikram <vvikram@andrew.cmu.edu>
2021-12-08 17:08:32 -0500

    change csv test harness to return string

--- src/test/java/diff/CommonsCSVTest.java
+++ src/test/java/diff/CommonsCSVTest.java
@@ -1,5 +1,6 @@
 package diff;
 
+import cmu.pasta.mu2.diff.Comparison;
 import cmu.pasta.mu2.diff.Diff;
 import cmu.pasta.mu2.diff.Mu2;
 
@@ -12,15 +13,22 @@ import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.junit.Assume;
 import org.junit.runner.RunWith;
 
 @RunWith(Mu2.class)
 public class CommonsCSVTest {
 
     @Diff
-    public List<CSVRecord> testCSVParser(@From(AsciiStringGenerator.class) String input) throws IOException {
-        CSVParser parser = CSVParser.parse(input, CSVFormat.EXCEL);
-        return parser.getRecords();
+    public String testCSVParser(@From(AsciiStringGenerator.class) String input) {
+        List<CSVRecord> records = null;
+        try {
+            CSVParser parser = CSVParser.parse(input, CSVFormat.EXCEL);
+            records = parser.getRecords();
+        } catch (IOException e) {
+            Assume.assumeNoException(e);
+        }
+        return records.toString();
     }
 
     @Fuzz

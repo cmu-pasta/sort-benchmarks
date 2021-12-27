3740124e054306b441e2a098f70938a1d0834b1f
Bella Laybourn <ilaybour@andrew.cmu.edu>
2021-12-27 10:58:56 -0800

    add equals for commons-cli

--- src/test/java/diff/CommonsCLITest.java
+++ src/test/java/diff/CommonsCLITest.java
@@ -11,13 +11,16 @@ import org.apache.commons.cli.*;
 import org.junit.Assume;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 
 @RunWith(Mu2.class)
 public class CommonsCLITest {
 
     private CommandLineParser parser = new DefaultParser();
 
-    @Diff
+    @Diff(cmp = "equalsCommandLine")
     public CommandLine testCommonsCLIParser(@From(OptionsGenerator.class) Options options, @From(AsciiStringGenerator.class) String input) {
         String[] args = new String[] { input };
         CommandLine cmd = null;
@@ -27,6 +30,13 @@ public class CommonsCLITest {
             Assume.assumeNoException(e);
         }
         return cmd;
+    }
+
+    @Comparison
+    public static Boolean equalsCommandLine(CommandLine cl1, CommandLine cl2) {
+        if(cl1 == cl2) return true;
+        if(cl1 == null || cl2 == null) return false;
+        return Objects.equals(cl1.getArgList(), cl2.getArgList()) && Arrays.equals(cl1.getOptions(), cl2.getOptions());
     }
 
     @Fuzz
